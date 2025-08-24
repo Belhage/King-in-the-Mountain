471038eb182e3945840d4cfc0aaf94375f76ddd3
Jack Conradsen <46312671+jack27121@users.noreply.github.com>
2025-08-24 12:52:51 +0200

    tilemap toughness array

--- Scenes and Scripts/Main Scene/world_tile_map.gd
+++ Scenes and Scripts/Main Scene/world_tile_map.gd
@@ -1,9 +1,16 @@
 extends TileMapLayer
 
-func _ready() -> void:
-	
-	pass
+var grid : Array
 
-func decrease_toughness(value: float, pos: Vector2i) -> void:
-	
-	pass
+func _ready() -> void:
+	var rect := get_used_rect()
+	for y in range(rect.position.y, rect.end.y):
+		for x in range(rect.position.x, rect.end.x):
+			var data = get_cell_tile_data(Vector2i(x,y))
+			if data:
+				grid[x][y] = data.get_custom_data("toughness")
+
+func decrease_toughness(value: int, pos: Vector2i) -> void:
+	grid[pos.x][pos.y] -= value
+	if(grid[pos.x][pos.y] <= 0):
+		erase_cell(pos)
