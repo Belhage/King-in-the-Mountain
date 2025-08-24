b5d1298a52e1c13a0aa9539a76b23c5f7d33d817
Jack Conradsen <46312671+jack27121@users.noreply.github.com>
2025-08-24 15:09:34 +0200

    hud

--- Scenes and Scripts/Menus/hud.gd
+++ Scenes and Scripts/Menus/hud.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+@export var inventory : int = 0
+@export var inventory_max : int = 100
+
+@export var blues : int = 0
+@export var pinks : int = 0
+@export var oranges : int = 0
+
 func _process(delta: float) -> void:
 	get_node("/root/")
 	pass
